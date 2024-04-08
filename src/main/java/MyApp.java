dca3732663eee4cec9e6d0c3520a71583aa83c13
kaveripriya <kaveri.priya@revature.com>
2024-04-08 20:51:35 +0530

    Files added

---
+++ src/main/java/MyApp.java
@@ -0,0 +1,13 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MyApp {
+    private static final Logger logger = LoggerFactory.getLogger(MyApp.class);
+
+    public static void main(String[] args) {
+        logger.debug("Debug message");
+        logger.info("Info message");
+        logger.warn("Warning message");
+        logger.error("Error message");
+    }
+}
